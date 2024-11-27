f6f1af67bdf7c08eb949fbd92190fa0caedc8c96
Luisfj98 <luisflorezjuarez@gmail.com>
2024-11-27 23:25:04 +0100

    Configuración de los reportes

---
+++ models/embedConfig2.js
@@ -0,0 +1,9 @@
+class EmbedConfig2 {
+    constructor(type, reportsDetail, embedToken) {
+        this.type = type;
+        this.reportsDetail = reportsDetail;
+        this.embedToken = embedToken;
+    }
+}
+
+module.exports = EmbedConfig2;
